61bdd41cda183acb3b7e0fb9a2c55c8747555253
aidaidaid <aidashirinovaaa@gmail.com>
2021-08-24 11:41:58 +0400

    fix

--- script8.js
+++ script8.js
@@ -1,11 +1,7 @@
-let first = factory(15);
-const arr = [30, 45, 60];
+const first = factory(15);
 
 function factory(n) {
     return function first(arr) {
-        return arr = arr.map(function (el) {
-            return el / n;
-        });
+        return arr = arr.map((el) => el / n);
     }
 }
-console.log(first(arr));
